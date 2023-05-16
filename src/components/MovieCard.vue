91493499ae60a6881fd718cbf16f637b2e80fe12
Kevin <kbalog124@gmail.com>
2023-05-17 01:03:39 +0200

    ajout du composant MovieCard

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+  description: string
+  image: string
+  url: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div
+    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+  >
+    <a href="#">
+      <img class="rounded-t-lg" :src="image" alt="" />
+    </a>
+    <div class="p-5">
+      <a href="#">
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          {{ title }}
+        </h5>
+      </a>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        {{ description }}
+      </p>
+      <RouterLink :to="`/movie/${url}`">
+        <a
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          En savoir plus
+        </a>
+      </RouterLink>
+    </div>
+  </div>
+</template>
