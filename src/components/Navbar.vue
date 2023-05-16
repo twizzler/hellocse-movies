3c3bc0113ab3241a8fb9105e349585ae81c82c3b
Kevin <kbalog124@gmail.com>
2023-05-17 00:13:05 +0200

    ajout de la barre de navigation et du view home

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="fixed left-0 top-0 w-full bg-blue-500 shadow-lg z-50">
+    <nav class="flex items-center justify-between gap-4 text-white py-6 w-full">
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-10">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">HelloCSE Movies</p>
+        </div>
+      </RouterLink>
+    </nav>
+  </header>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: {
+        title: 'Films populaires',
+      },
+    },
+  ],
 })
