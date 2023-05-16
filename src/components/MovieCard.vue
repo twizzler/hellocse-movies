cf130cabd3f701365d36b5cd3a0d810db67c2a11
Kevin <kbalog124@gmail.com>
2023-05-17 01:51:30 +0200

    ajustements description MovieCard

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -11,23 +11,26 @@ const props = defineProps<Props>()
 
 <template>
   <div
-    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+    class="flex flex-col max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
   >
     <a href="#">
       <img class="rounded-t-lg" :src="image" alt="" />
     </a>
-    <div class="p-5">
+    <div class="flex flex-col p-5 flex-grow">
       <a href="#">
         <h5
-          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white"
         >
           {{ title }}
         </h5>
       </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        {{ description }}
+      <p
+        class="description overflow-hidden text-overflow-ellipsis h-12 mb-3 font-normal text-gray-700 dark:text-gray-400 flex-grow"
+      >
+        <span v-if="description">{{ description }}</span>
+        <span v-else>Aucune description...</span>
       </p>
-      <RouterLink :to="`/movie/${url}`">
+      <RouterLink :to="`/movie/${url}`" class="mt-auto">
         <a
           class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
